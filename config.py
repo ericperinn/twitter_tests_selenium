9a24a29b4897b574e524da6316fbd0b1ababaa6f
Eric Perin <ericd.perin11@gmail.com>
2024-07-24 15:24:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 TWITTER_URL = "https://twitter.com"
 LOGIN_URL = f"{TWITTER_URL}/i/flow/login"
 
-USER_EMAIL = "albertotesteap2"
-USER_PASSWORD = "testeap2!"
+USER_EMAIL = "*******"
+USER_PASSWORD = "*****"
